361a042e89d0d74cbb40677f3acb43570c16edd6
Tony Shifflett <charles.l.shifflett@gmail.com>
2024-12-07 17:05:40 -0600

    created webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './public/scripts/index.js', 
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', 
+      filename: 'index.html', 
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/pages', to: 'pages' }, 
+        { from: 'public/styles', to: 'styles' }, 
+      ],
+    }),
+  ],
+};
